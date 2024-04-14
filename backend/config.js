c2b1cdf9d37e3cf1d8720c26ca8da4fdb672108c
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-04-14 12:22:46 +0300

    added mongoURL to config

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 8000;
+
+export const mongoURL =
+  'mongodb+srv://paskaeu25:Test.123@cluster0.d65rhiv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
