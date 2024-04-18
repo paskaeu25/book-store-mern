4a29b3f614c09efecbd124710491904df25a3a29
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-04-18 09:10:33 +0300

    set upo react router dom. Added browser router components to main.jsx and Routes to App components

---
+++ frontend/src/pages/DeleteBook.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const deleteBook = () => {
+  return <div>deleteBook</div>;
+};
+
+export default deleteBook;
---
+++ frontend/src/pages/EditBook.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const editBook = () => {
+  return <div>editBook</div>;
+};
+
+export default editBook;
