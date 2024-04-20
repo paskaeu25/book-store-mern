07ca82daf390bb338e51d4b77699a55029d0a5c3
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-04-20 11:49:42 +0300

    changed cors config and installed axios with react icons

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,14 @@ const app = express();
 app.use(express.json());
 
 // Middleware for CORS
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
