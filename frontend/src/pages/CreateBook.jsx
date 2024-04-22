cf81a010c504d4e51f19f4c57001422060ca75fa
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-04-22 16:47:44 +0300

    created the function to handle the adding of new book

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -10,6 +10,22 @@ const createBook = () => {
   const [publishYear, setPublishYear] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios.post('http://localhost:8000/books', data).then(() => {
+      setLoading(false);
+      navigate('/').catch((error) => {
+        setLoading(false);
+        alert('An error occured. Please check the console');
+        console.log(error);
+      });
+    });
+  };
 
   return <div>createBook</div>;
 };
