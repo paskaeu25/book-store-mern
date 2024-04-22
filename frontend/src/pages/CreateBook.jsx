46871b8f66f628fa14fdb6db89f19c906b8e009c
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-04-22 16:41:24 +0300

    imported components and libraries to create Book component. Also defined variables and functions for useState

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const createBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
   return <div>createBook</div>;
 };
 
