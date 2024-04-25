f7b73e2f208a2236050bea2b41fbf051bf44d4fc
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-04-25 21:03:21 +0300

    small typo

--- frontend/src/components/BookSingleCard.jsx
+++ frontend/src/components/BookSingleCard.jsx
@@ -7,10 +7,7 @@ import { MdOutlineDelete } from 'react-icons/md';
 
 const BookSingleCard = ({ book }) => {
   return (
-    <div
-      key={book._id}
-      className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
-    >
+    <div className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
       <h2 className="bg-red-300 rounded-lg px-4 py-1 absolute top-1 right-2">
         {book.publishYear}
       </h2>
