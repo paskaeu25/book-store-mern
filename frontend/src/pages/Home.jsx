fb98d48e89a6012c915a997d052ad4541b031620
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-04-25 20:16:11 +0300

    added table and card buttons to homepage.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,6 +9,7 @@ import BooksTable from '../components/home/BooksTable';
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState('table');
   useEffect(() => {
     setLoading(true);
     axios
@@ -25,13 +26,33 @@ const Home = () => {
 
   return (
     <div className="p-4">
+      <div className="flex justify-center items-center gap-x-4">
+        <button
+          className="bg-sky-300 hover:bg-sky-600 hover:text-white px-4 py-1 rounded-lg"
+          onClick={() => setShowType('table')}
+        >
+          Table
+        </button>
+        <button
+          className="bg-sky-300 hover:bg-sky-600 hover:text-white px-4 py-1 rounded-lg"
+          onClick={() => setShowType('card')}
+        >
+          Card
+        </button>
+      </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-4xl text-sky-800" />
         </Link>
       </div>
-      {loading ? <Spinner /> : <BooksTable books={books} />}
+      {loading ? (
+        <Spinner />
+      ) : showType === 'table' ? (
+        <BooksTable books={books} />
+      ) : (
+        <BooksCard />
+      )}
     </div>
   );
 };
