5f72eec1e1b720b81f4306e8014d7f9cdb2b772d
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-04-25 09:29:04 +0300

    started delete book page. set the states and created handle delete function up to delete request with then block

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const deleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios.delete(`http://localhost:8000/books/${id}`).then(() => {
+      setLoading(false);
+      navigate('/');
+    });
+  };
+
   return <div>deleteBook</div>;
 };
 
