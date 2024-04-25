0a155b6bd55c79f37167a7eae1019b00aee40b53
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2024-04-25 19:52:52 +0300

    added catch block and wrote the html to be rendered for delete page. Completed the delete page

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -10,13 +10,35 @@ const deleteBook = () => {
   const { id } = useParams();
   const handleDeleteBook = () => {
     setLoading(true);
-    axios.delete(`http://localhost:8000/books/${id}`).then(() => {
-      setLoading(false);
-      navigate('/');
-    });
+    axios
+      .delete(`http://localhost:8000/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error occured. Please check the console ');
+        console.log(error);
+      });
   };
 
-  return <div>deleteBook</div>;
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">Are you sure you want to delete this book?</h3>
+        <button
+          className="p-4 bg-red-600 m-8 text-white w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default deleteBook;
